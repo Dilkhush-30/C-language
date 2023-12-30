1ff9e6136e41708dd885c8e1a7f9eada60934e00
Dilkhush-30 <dilkhushkumar25126@gmail.com>
2023-12-30 18:36:04 +0530

    new question

---
+++ Loops/loop_question_07.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        
+        for(int j=1;j<i;j++){
+            printf(" ");
+        }
+        int k = 5;
+        while (k>=i)
+        {
+            printf("* ");
+            k--;
+        }
+        
+        
+        printf("\n");
+    }
+    return 0;
+
+}
