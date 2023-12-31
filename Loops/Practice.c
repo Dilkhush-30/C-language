b222355e8bc085408d19ef80a4f1baeea4c89e10
Dilkhush-30 <dilkhushkumar25126@gmail.com>
2023-12-31 15:13:08 +0530

    new question

---
+++ Loops/Practice.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("enter the n number:");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+printf("%d is a even number",num);
+    }
+    return 0;
+}
