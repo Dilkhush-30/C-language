48a94bb01ea184c036039dcb9c0f5d50e44c1530
Dilkhush-30 <dilkhushkumar25126@gmail.com>
2024-01-24 22:29:54 +0530

    new questions

---
+++ Loops/loop_question_15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0; i<=4; i++)
+    {
+        for(int j=3; j>=i; j--)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=(i*2)+1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+} 
